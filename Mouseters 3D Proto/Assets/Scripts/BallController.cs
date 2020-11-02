932e0ea384c9744014ff0d268fd69bfd4a9041af
axu11 <50363222+axu11@users.noreply.github.com>
2020-11-02 00:09:54 -0800

    unbroke

--- Mouseters 3D Proto/Assets/Scripts/BallController.cs
+++ Mouseters 3D Proto/Assets/Scripts/BallController.cs
@@ -64,4 +64,10 @@ public class BallController : MonoBehaviour
     void shoot(Vector3 dir) {
         this.GetComponent<Rigidbody>().AddForce(dir * 500);
     }
+
+    void OnTriggerEnter (Collider other) {
+        if(other.tag == "Hole") {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
 }
